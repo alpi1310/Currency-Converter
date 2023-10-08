33e600bee5fa082bbf54a42094ef122fe0d11596
Alka Pal <alkapal1308@gmail.com>
2023-10-09 00:41:16 +0530

    commit all the files

---
+++ script.js
@@ -0,0 +1,32 @@
+var select = document.querySelectorAll('.Currency');
+input_currency = document.getElementById('input_currency');
+output_currency = document.getElementById('output_currency');
+
+
+const host = 'api.frankfurter.app';
+fetch(`https://${host}/latest`)
+    .then(data => data.json())
+    .then((data) => {
+        // console.log(entries)
+        const entries = Object.entries(data.rates);
+        for (i = 0; i < entries.length; i++) {
+            select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+            select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+        }
+    });
+
+
+function converter() {
+    var input_currency_val = input_currency.value;
+    if (select[0].value != select[1].value) {
+        // alert("Right");
+        const host = 'api.frankfurter.app';
+        fetch(`https://${host}/latest?amount=${input_currency_val}&from=${select[0].value}&to=${select[1].value}`)
+            .then(val => val.json())
+            .then((val) => {
+                output_currency.value=Object.values(val.rates)[0];
+            });
+    } else {
+        alert("Please select two diffrent currency")
+    }
+}
